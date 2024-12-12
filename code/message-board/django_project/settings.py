7657b2c6ada46e2e499285ef10a4878236fd03fd
MASA20201203 <koedame032@gmail.com>
2024-12-12 18:11:47 +0900

    update: ch5 p102 デプロイ用ファイルを追加

--- code/message-board/django_project/settings.py
+++ code/message-board/django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-5ftp*(zql)=_f-ch2(-8&@qf7$fx0lja#2wkapitd*b=%n&ruh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
